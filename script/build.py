1c41a272f26b38d9f0be9b1878044ab637d6ddce
LoafMan <95560810+loafman-kangjun@users.noreply.github.com>
2024-10-20 18:39:19 +0800

    Update build.py

--- script/build.py
+++ script/build.py
@@ -34,38 +34,13 @@ def main():
     'skia_enable_skottie=true'
   ]
 
-  if 'macos' == system:
-    args += [
-      'skia_use_system_freetype2=false',
-      # 'skia_enable_gpu=true',
-      # 'skia_use_gl=true',
-      'skia_use_metal=true',
-      'extra_cflags_cc=["-frtti"]'
-    ]
-    if 'arm64' == machine:
-      args += ['extra_cflags=["-stdlib=libc++"]']
-    else:
-      args += ['extra_cflags=["-stdlib=libc++", "-mmacosx-version-min=10.13"]']
-  elif 'linux' == system:
-    args += [
-      'skia_use_system_freetype2=true',
-      # 'skia_enable_gpu=true',
-      # 'skia_use_gl=true',
-      'extra_cflags_cc=["-frtti"]',
-      'cxx="g++-9"',
-    ]
-  elif 'windows' == system:
-    args += [
-      'skia_use_system_freetype2=false',
-      # 'skia_use_angle=true',
-      'skia_use_direct3d=true',
-      'extra_cflags=["-DSK_FONT_HOST_USE_SYSTEM_SETTINGS"]',
-    ]
-  elif 'android' == system:
-    args += [
-      'skia_use_system_freetype2=false',
-      'ndk="'+ ndk + '"'
-    ]
+
+  args += [
+     'skia_use_system_freetype2=false',
+     # 'skia_use_angle=true',
+     'skia_use_direct3d=true',
+     'extra_cflags=["-DSK_FONT_HOST_USE_SYSTEM_SETTINGS"]',
+  ]
 
   out = os.path.join('out', build_type + '-' + machine)
   gn = 'gn.exe' if 'windows' == system else 'gn'
